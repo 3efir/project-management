6f153219838099eafb41784ef987b711e64f9647
Oleksandr Fihun <oleksandr.fihun@globallogic.com>
2024-06-19 10:37:05 +0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const baseConfig = {
+	"semi": true,
+	"singleQuote": true,
+	"trailingComma": "all",
+	"tabWidth": 4,
+	"endOfLine": "lf",
+	"printWidth": 100
+};
+module.exports = {
+  	...baseConfig,
+};
